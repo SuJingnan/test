f520175d30ad6ca8a10625ccf99412a09c7b795e
SUJINGN <jingnan.su@hotmail.com>
2018-04-04 18:43:36 +0800

    change test

--- ConsoleApplication1/ConsoleApplication2/Program.cs
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -8,8 +8,8 @@ namespace ConsoleApplication2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Maieeeeddn(string[] args)
         {
-        }sss
+        } 
     }
 }
